0558b2044fd458e27575049774ef9f778d565c51
darkllen <yankiniigor@gmail.com>
2024-02-27 16:33:49 +0200

    Use module

--- main.tf
+++ main.tf
@@ -12,31 +12,18 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
+  name = "YankinLab5v3"
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t3.micro"
-  security_groups = ["ssh-http-https"]
-  key_name        = "yankin-key"
+  instance_type          = "t3.micro"
+  key_name               = "yankin-key"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-01142ec3c87ec19dd"]
 
   tags = {
-    Name = "YankinLab5v2"
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
-
-
